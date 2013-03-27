e410ab86438464dd72645c8d723534362a4ee2fb
Tom Gouder <tomgouder@gmail.com>
2013-03-27 22:44:29 +0100

    Created Trexr Application and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trexr::Application.config.secret_token = '0f34d3f3c9458d4a3b094f3bafbdaa3720dc3dc5f6520ae34558712acfa6ec8ad5a2c9eeb2314e865a11b6f7f37fda0af98572a797df842e046a57dd9ca8d172'
